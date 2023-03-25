6505fd5070f3e08c5e0f8a43fad4436c43b579b8
zibzikon <100312668+zibzikon@users.noreply.github.com>
2023-03-25 17:47:52 +0200

    Fix: Changed NotImplementsInterfaceException realization

--- Assets/Foundation/Utils/Exceptions/NotImplementsInterfaceException.cs
+++ Assets/Foundation/Utils/Exceptions/NotImplementsInterfaceException.cs
@@ -4,17 +4,7 @@ namespace Foundation.Exceptions
 {
     public class NotImplementsInterfaceException : Exception
     {
-        private readonly Type _sourceType;
-        private readonly Type _interfaceType;
-
-        public NotImplementsInterfaceException(Type sourceType, Type interfaceType)
-        {
-            _sourceType = sourceType;
-            _interfaceType = interfaceType;
-        }
-
-        public override string ToString()
-            => $"The type ({_sourceType}) is not implements ({_interfaceType}) interface";
-
+        public NotImplementsInterfaceException(Type sourceType, Type interfaceType) :
+            base($"The type ({sourceType}) is not implements ({interfaceType}) interface") { }
     }
 }
